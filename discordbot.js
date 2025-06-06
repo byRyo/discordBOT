1fc937b01c135bb141ca0aceb0e09e5d35c1c5a8
Ryo <rrblx511@gmail.com>
2025-06-06 21:32:57 +0200

    Update discordbot.js

--- discordbot.js
+++ discordbot.js
@@ -1,24 +1,25 @@
-const { Client, GatewayIntentsBits } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
     intents: [
-        GatewayIntentsBits.Guilds,
-        GatewayIntentsBits.GuildMessages,
-        GatewayIntentsBits.MessageContent
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
     ]
 });
 
 const token = '';
 
-client.on('ready', () =>{
-    console.log('${client.user.tag} succefully loaded !')
+client.on('ready', () => {
+    console.log(`${client.user.tag} successfully loaded !`);
 });
-client.on('messageCreate', message => {
-  if (message.author.bot) return;
 
-  if (message.content === 'hello') {
-    message.reply('hello');
-  }
+client.on('messageCreate', message => {
+    if (message.author.bot) return;
+
+    if (message.content === 'hello') {
+        message.reply('hello');
+    }
 });
 
 client.login(token);
